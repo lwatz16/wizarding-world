ce53007dc190647f18c2d557d8c99030efe3c718
lswatson16 <lauralynw16@gmail.com>
2022-04-19 20:26:10 -0400

    Update display of Card and import stylesheet

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ name, image, house }) => {
   return(
-    <div>
+    <div className='card'>
+      {/* <img src={`${image}`} alt={`${name}`}></img> */}
       <h2>{name}</h2>
-      
+      {/* <h3>{house}</h3> */}
     </div>
   )
 }
