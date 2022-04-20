b33effa50055d1f7a6ab4fd3aac88703a8a9f73e
lswatson16 <lauralynw16@gmail.com>
2022-04-20 10:57:48 -0400

    Add and import Header stylesheet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
   return(
     <header>
-      <h1>Wizarding World</h1> 
+      <h1 className='logo-title'>Wizarding World</h1> 
     </header>
   )
 }
