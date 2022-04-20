6984cd56b98fdb90d121cac40ba86d7c2503417c
lswatson16 <lauralynw16@gmail.com>
2022-04-19 20:25:40 -0400

    Update the key property in the list of characters and import stylesheet

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './CharacterList.css';
 
 const CharacterList = ({ characters }) => {
-  const characterCards = characters.map((character) => {
+  const characterCards = characters.map((character, index) => {
     return(
       <Card
         name={character.name}
         image={character.image}
         house={character.house}
-        key={character.name}
+        key={index}
       />
     )
   }) 
 
   return(
-    <div>
+    <div className='list-container'>
       {characterCards}
     </div>
   )
