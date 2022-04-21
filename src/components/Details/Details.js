f96e80799f8575cad13e279b0b28e9fc99208370
lswatson16 <lauralynw16@gmail.com>
2022-04-21 15:49:14 -0400

    Implement router on back button to return to home page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Details.css';
 
-const Details = () => {
+const Details = (props) => {
+  console.log('props in dets', props)
   return(
-    <>Details HERE</>
+    <main className='details-page'>
+      <header className='.details-header'>
+        <h1 className='.details-heading'>Wizarding World</h1>   
+      </header>
+
+      <Link to={'/'}>
+        <button className='.back-btn'>Back</button>
+      </Link>
+
+    </main>
   )
 }
 
