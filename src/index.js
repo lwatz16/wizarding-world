242f1c8dfba0ad3ca8ada4866938bce87a62b533
lswatson16 <lauralynw16@gmail.com>
2022-04-21 12:10:36 -0400

    Install and implement browser router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
 createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
