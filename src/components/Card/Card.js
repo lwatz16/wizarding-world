81b162d2b9cead87faf8b39e53deda1b0df7f0dc
lswatson16 <lauralynw16@gmail.com>
2022-04-21 01:20:09 -0400

    Convert listed character names to buttons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,13 @@ import './Card.css'
 const Card = ({ name }) => {
   return(
     <div className='card'>
-      <h2>{name}</h2>
+      {/* <h2>{name}</h2> */}
+      <button 
+        className='card-btn'
+        name={name}
+        onClick={(e)=> console.log('click', e.target)}
+      >{name}
+      </button>
     </div>
   )
 }
