476b4321cb9641837d192bfd0b96f6640c55cf79
lswatson16 <lauralynw16@gmail.com>
2022-04-21 10:20:55 -0400

    Revise styling of character cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,15 +3,12 @@ import './Card.css'
 
 const Card = ({ name }) => {
   return(
-    <div className='card'>
-      {/* <h2>{name}</h2> */}
       <button 
         className='card-btn'
         name={name}
         onClick={(e)=> console.log('clicked>>>', e.target.name)}
       >{name}
       </button>
-    </div>
   )
 }
 
