f9796937b099c757a91037595fc7af798cddd209
lswatson16 <lauralynw16@gmail.com>
2022-04-22 15:46:52 -0400

    Restyle NoMatch component

--- src/components/NoMatchPath/NoMatchPath.js
+++ src/components/NoMatchPath/NoMatchPath.js
@@ -3,9 +3,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NoMatchPath = () => (
-  <div className='no-match'>
-    <h1>404 error - Page not found</h1>
-    <Link to='/'>Home</Link>
+  <div className='no-match'
+    style={{
+      backgroundImage: 'url(/platform9-unsplash.jpg)',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}
+  >
+    <div className='no-match-error'>
+      <h1>404 error</h1>
+      <h2>Page not found</h2>
+      <Link to='/'>
+        <button>Home</button>
+      </Link>
+    </div>
   </div>
 );
 
