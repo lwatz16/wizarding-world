f1b9bab68223f00c2dd5a33cef33ac4cb770a6d0
lswatson16 <lauralynw16@gmail.com>
2022-04-24 11:33:59 -0400

    Convert function component to a class component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,51 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import './Details.css';
-import NoMatchPath from '../NoMatchPath/NoMatchPath';
+// import NoMatchPath from '../NoMatchPath/NoMatchPath';
 
-const Details = ({ character }) => {
-  return(
-    <>
-      {!character.name && <NoMatchPath />}
-      {character.name && 
-        <main className='details-page'>
-          <header className='.details-header'>
-            <h1 className='.details-heading'>Wizarding World</h1>
-          </header>
+class Details extends Component {
+  render() {
+    const {character} = this.props
+    return(
+      <>
+        {/* {!character.name && <Redirect to='/' />} */}
+        {character.name && 
+          <main className='details-page'>
+            <header className='.details-header'>
+              <h1 className='.details-heading'>Wizarding World</h1>
+            </header>
+    
+            <Link to={'/'}>
+              <button className='.back-btn'>Back</button>
+            </Link>
+    
+            <section className='details-section'>
+              <h2 className='details-name'>{character.name}</h2>
+              {character.image && <div className='img-container'>
+                <img className='details-img' src={character.image} alt={character.name}></img>
+              </div>
+              }
+              <div className='details-container'>
+                <p>House: {character.house}</p>
+                <p>Ancestry: {character.ancestry}</p>
+                <p>Species: {character.species}</p>
+                <p>Patronus: {character.patronus}</p>
+                <p>Wizard: {character.wizard ? 'true' : 'false'}</p>
+                <p>Staff: {character.hogwartsStaff ? 'true' : 'false'}</p>
+                <p>Student: {character.hogwartsStudent ? 'true' : 'false'}</p>
+                <p>Actor: {character.actor}</p>
+              </div>
+              
+              {character.wand.wood && <div className='wand-details'>
+                <h3>Wand</h3>
+                <p>Wood: {character.wand.wood}</p>
+                <p>Core: {character.wand.core}</p>
+                <p>Length: {character.wand.length}</p>
+              </div>
+              }
+            </section>
+          </main>
+        }
+      </>
+    )
 
-          <Link to={'/'}>
-            <button className='.back-btn'>Back</button>
-          </Link>
-
-          <section className='details-section'>
-            <h2 className='details-name'>{character.name}</h2>
-            {character.image && <div className='img-container'>
-              <img className='details-img' src={character.image} alt={character.name}></img>
-            </div>
-            }
-            <div className='details-container'>
-              <p>House: {character.house}</p>
-              <p>Ancestry: {character.ancestry}</p>
-              <p>Species: {character.species}</p>
-              <p>Patronus: {character.patronus}</p>
-              <p>Wizard: {character.wizard ? 'true' : 'false'}</p>
-              <p>Staff: {character.hogwartsStaff ? 'true' : 'false'}</p>
-              <p>Student: {character.hogwartsStudent ? 'true' : 'false'}</p>
-              <p>Actor: {character.actor}</p>
-            </div>
-            
-            {character.wand.wood && <div className='wand-details'>
-              <h3>Wand</h3>
-              <p>Wood: {character.wand.wood}</p>
-              <p>Core: {character.wand.core}</p>
-              <p>Length: {character.wand.length}</p>
-            </div>
-            }
-          </section>
-        </main>
-      }
-    </>
-  )
+  }
 }
 
 export default Details;
