d1edb8f5c0aeefa8a94f73296f44e001832778d6
lswatson16 <lauralynw16@gmail.com>
2022-04-24 11:13:18 -0400

    Update API endpoint to get characters

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,18 @@
 const apiCalls = {
   getCharacters() {
-    return fetch('http://hp-api.herokuapp.com/api/characters')
-      .then(response => response.json())
+    return fetch('https://cfhsik8pad.execute-api.us-east-1.amazonaws.com/dev/characters')
+      .then(response => {
+        console.log('res', response)
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        return data
+      })
+      .catch(error => {
+        console.log(error)
+        throw error
+      })
   }
 }
 
